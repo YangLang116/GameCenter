528277d1d1821d34c98c7ecc8c9f2d901451cc2b
YangLang <yangl@inke.cn>
2022-05-30 13:51:44 +0800

    去掉RunOnce逻辑

--- src/main/java/com/xtu/plugin/game/lifecycle/AppLifecycleListenerImpl.java
+++ src/main/java/com/xtu/plugin/game/lifecycle/AppLifecycleListenerImpl.java
@@ -1,7 +1,6 @@
 package com.xtu.plugin.game.lifecycle;
 
 import com.intellij.ide.AppLifecycleListener;
-import com.intellij.ide.util.RunOnceUtil;
 import com.xtu.game.base.GameCenterFacade;
 import com.xtu.game.base.GameCenterServiceProvider;
 import com.xtu.game.base.service.AudioService;
@@ -17,23 +16,21 @@ public class AppLifecycleListenerImpl implements AppLifecycleListener {
 
     @Override
     public void appFrameCreated(@NotNull List<String> commandLineArgs) {
-        RunOnceUtil.runOnceForApp("register GameCenter Service", () -> {
-            //注册服务
-            GameCenterFacade gameCenterFacade = GameCenterFacade.getInstance();
-            gameCenterFacade.injectService(AudioService.class, new GameCenterServiceProvider<>() {
-                @Override
-                public AudioService create() {
-                    return new AudioServiceImpl();
-                }
-            });
-            gameCenterFacade.injectService(StorageService.class, new GameCenterServiceProvider<>() {
-                @Override
-                public StorageService create() {
-                    return new StorageServiceImpl();
-                }
-            });
-            //加载游戏列表
-            ConfLoader.getInstance().load();
+        //注册服务
+        GameCenterFacade gameCenterFacade = GameCenterFacade.getInstance();
+        gameCenterFacade.injectService(AudioService.class, new GameCenterServiceProvider<>() {
+            @Override
+            public AudioService create() {
+                return new AudioServiceImpl();
+            }
         });
+        gameCenterFacade.injectService(StorageService.class, new GameCenterServiceProvider<>() {
+            @Override
+            public StorageService create() {
+                return new StorageServiceImpl();
+            }
+        });
+        //加载游戏列表
+        ConfLoader.getInstance().load();
     }
 }
